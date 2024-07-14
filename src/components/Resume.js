46c63b43189c3499777ffe1b138780e4d1589979
Abrar Khalil <abrarhosny55@gmail.com>
2024-07-14 20:33:27 +0200

    Handle hover & update title

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,24 +1,20 @@
 import React from 'react'
-import styled from 'styled-components';
-
-const Title = styled.h2 ` &::after { left: calc(10rem + 145px) }`;
+import { Title } from "../components";
 
 const Resume = () => {
   return (
     <div>
-      <Title className=" title">
-        Resume / CV
-      </Title>
+      <Title title='Resume/CV' w='450px'/>
       <p className='text-lg text-gray-500'>
         You can download my resume and CV here.
       </p>
       <div className='mt-10 flex gap-4'>
-        <button className="btn btn-primary">
+        <button className="btn btn-primary hover:bg-accentColor hover:border-white hover:text-white transition-all duration-300">
           <a href="https://drive.google.com/file/d/10i9CoybwUMwYYdkKmxVM3d1Rv4KYGpvk/view?usp=sharing" target="_blank" rel="noopener noreferrer">
             Download Resume
           </a>
         </button>
-        <button className="btn btn-secondary" style={{ border: '1px solid var(--accent-color)'}}>
+        <button className="btn btn-secondary hover:border-accentColor hover:bg-white hover:text-accentColor transition-all duration-300" style={{ border: '2px solid var(--accent-color)'}}>
           <a href="https://drive.google.com/file/d/1GSlhahF-a3FYkJxXfmWfQxCDPRHIoUfz/view?usp=sharing" target="_blank" rel="noopener noreferrer">
             Download CV
           </a>
